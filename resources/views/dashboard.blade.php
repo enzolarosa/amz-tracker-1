f442c6295290949925dcf78992324a80be78025c
Vincenzo La Rosa <enzo@vincenzolarosa.it>
2020-07-25 11:30:31 +0200

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,6 @@
     <livewire:accuweather-five-day-forecast-tile position="b1:d2"/>
 
     <livewire:forge-server-tile position="a3:a10"/>
-
     <livewire:forge-recent-events-tile position="b5:b10"/>
 
     <livewire:chart-tile chartClass="{{\App\Charts\DailyUsersChart::class}}" position="c5:d6"/>
