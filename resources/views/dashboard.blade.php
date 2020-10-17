8a1d08e3dbf321b4dfe2b63d4544f6a29e20921e
Vincenzo La Rosa <enzo@vincenzolarosa.it>
2020-10-17 15:15:51 +0200

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,4 @@
 <x-dashboard>
     <livewire:accuweather-current-conditions-tile position="a1:a2"/>
     <livewire:accuweather-five-day-forecast-tile position="b1:d2"/>
-
-    <livewire:chart-tile chartClass="{{\App\Charts\DailyUsersChart::class}}" position="c5:d6"/>
-    <livewire:chart-tile chartClass="{{\App\Charts\DailyProductsChart::class}}" position="c7:d8"/>
-    <livewire:chart-tile chartClass="{{\App\Charts\DailyProductAnalyzeChart::class}}" position="c9:d10"/>
 </x-dashboard>
