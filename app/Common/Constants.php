fdac8e7ba131e0b0db8f946470899723013a3dc1
Vincenzo La Rosa <enzo@vincenzolarosa.it>
2020-10-28 15:29:13 +0100

    increse job timeout

--- app/Common/Constants.php
+++ app/Common/Constants.php
@@ -4,10 +4,7 @@ namespace App\Common;
 
 class Constants
 {
-    public static int $WAIT_CRAWLER = 10; // seconds
-    public static int $SLEEP_CRAWLER = 10; // seconds
-    public static int $TRACKER_TIMEOUT = 30; // minutes
-    public static int $WAIT_AMZ_HTTP_ERROR = 5;// minutes
+    public static int $TRACKER_TIMEOUT = 40; // minutes
     public static int $CONNECTION_TIMEOUT = 3;// minutes
 
     public static function getAmzCookiesKey(): string
